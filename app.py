7098bfc211a00193c9a30f8ed2b4079885d7c2a2
rafaelamendes <raafaelamendees@gmail.com>
2024-10-11 15:02:14 -0300

    Aula do dia 11/10

--- app.py
+++ app.py
@@ -24,3 +24,26 @@ def contato():
 @app.route("/sobre", methods=( 'GET', )) 
 def sobre():
     return "<h1>sobre</h1>"
+
+@app.route("/area")
+def area():
+    altura =  float(request.args.get('a'))
+    largura =  float(request.args.get('l'))
+    return f"""<h1> A área
+    informada >L={largura}*A={altura}
+    =>Area={largura+altura}</h1>"""
+
+@app.route("/par_ou_impar", methods=('GET',))
+def par_ou_impar():
+  numero = float(request.args.get('n'))
+  if numero % 2 == 0:
+    return f"O número {numero} é par."
+  else:
+    return f"O número {numero} é ímpar."
+  
+@app.route("/sobrenome", methods=('GET',))
+def nomesobrenome():
+  nome = request.args.get('nome')
+  sobrenome = request.args.get('sobrenome')
+  return f"""<h1> sobrenome </h1>
+  <p>{sobrenome},{nome}</p>"""
