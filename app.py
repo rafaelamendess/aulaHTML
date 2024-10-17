099c122a2728499550e1ba3f5795a89f5dc0616f
rafaelamendes <raafaelamendees@gmail.com>
2024-10-17 15:42:27 -0300

    potencia 17/10

--- app.py
+++ app.py
@@ -25,25 +25,25 @@ def contato():
 def sobre():
     return "<h1>sobre</h1>"
 
-@app.route("/area")
-def area():
-    altura =  float(request.args.get('a'))
-    largura =  float(request.args.get('l'))
+@app.route("/area/<float:largura>/<float:altura>", methods=( 'GET', ))
+def area(largura: float, altura: float):
     return f"""<h1> A área
-    informada >L={largura}*A={altura}
-    =>Area={largura+altura}</h1>"""
+    informada >L={largura} * A={altura}
+    => Area={largura*altura}</h1>"""
 
-@app.route("/par_ou_impar", methods=('GET',))
-def par_ou_impar():
-  numero = float(request.args.get('n'))
+@app.route("/parimpar/<float:numero>", methods=('GET',))
+def par_ou_impar(numero):
   if numero % 2 == 0:
     return f"O número {numero} é par."
   else:
     return f"O número {numero} é ímpar."
   
-@app.route("/sobrenome", methods=('GET',))
-def nomesobrenome():
-  nome = request.args.get('nome')
-  sobrenome = request.args.get('sobrenome')
+@app.route("/sobrenome/<string:nome>/<string:sobrenome>", methods=('GET',))
+def nomesobrenome(nome: str, sobrenome: str):
   return f"""<h1> sobrenome </h1>
   <p>{sobrenome},{nome}</p>"""
+
+@app.route("/potencia/<float:um>/<float:dois>", methods=( 'GET', ))
+def potencia(um: float, dois: float):
+    return f"""<h1>{um}^{dois}
+     ={um**dois}</h1>"""
